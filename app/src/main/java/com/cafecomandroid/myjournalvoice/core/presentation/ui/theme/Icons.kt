aa76b1cff192a0147c5bb1b2c55462575011b6ce
lucas-marciano <lucas.almeida.marciano@gmail.com>
2025-05-19 17:16:37 -0300

    feat: implementado valores do thema

---
+++ app/src/main/java/com/cafecomandroid/myjournalvoice/core/presentation/ui/theme/Icons.kt
@@ -0,0 +1,15 @@
+package com.cafecomandroid.myjornalvoice.core.presentation.ui.theme
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.cafecomandroid.myjornalvoice.R
+
+val Icons.Filled.Pause: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.pause)
+
+val Icons.Filled.Microphone: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.microphone)
