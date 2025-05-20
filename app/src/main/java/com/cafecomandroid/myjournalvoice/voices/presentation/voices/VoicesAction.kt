a91ec103eda5e77bf76f5139c01b7dc222eb02d0
lucas-marciano <lucas.almeida.marciano@gmail.com>
2025-05-20 17:27:21 -0300

    feat: implementado novos fluxos de states e actions

--- app/src/main/java/com/cafecomandroid/myjournalvoice/voices/presentation/voices/VoicesAction.kt
+++ app/src/main/java/com/cafecomandroid/myjournalvoice/voices/presentation/voices/VoicesAction.kt
@@ -1,10 +1,15 @@
 package com.cafecomandroid.myjournalvoice.voices.presentation.voices
 
+import com.cafecomandroid.myjournalvoice.voices.presentation.models.MoodUI
 import com.cafecomandroid.myjournalvoice.voices.presentation.voices.models.VoicesFilter
 
 sealed interface VoicesAction {
     data object OnMoodChipClick : VoicesAction
+    data object OnDismissMoodChipDropDown : VoicesAction
+    data class OnFilterByMoodClick(val moodUI: MoodUI) : VoicesAction
     data object OnTopicChipClick : VoicesAction
+    data object OnDismissTopicChipDropDown : VoicesAction
+    data class OnFilterByTopicClick(val topic: String) : VoicesAction
     data object OnFabClick : VoicesAction
     data object OnFabLongClick : VoicesAction
     data object OnSettingsClick : VoicesAction
